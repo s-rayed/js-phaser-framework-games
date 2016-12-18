bda6474b4b8bc9e8da9c055c6b7b7a8c5264e945
Rayed <siddiqui.rayed@gmail.com>
2016-12-18 15:53:00 -0500

    added sound and finishing touches

--- farm/js/main.js
+++ farm/js/main.js
@@ -8,12 +8,19 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO);
 var GameState = {
   preload: function(){
     this.load.image('background', 'assets/images/background.png');
-    this.load.image('chicken', 'assets/images/chicken.png');
-    this.load.image('horse', 'assets/images/horse.png');
-    this.load.image('pig', 'assets/images/pig.png');
-    this.load.image('sheep', 'assets/images/sheep3.png');
     this.load.image('arrow', 'assets/images/arrow.png');
+
+    this.load.spritesheet('chicken', 'assets/images/chicken_spritesheet.png', 131, 200, 3);
+    this.load.spritesheet('horse', 'assets/images/horse_spritesheet.png', 212, 200, 3);
+    this.load.spritesheet('pig', 'assets/images/pig_spritesheet.png', 297, 200, 3);
+    this.load.spritesheet('sheep', 'assets/images/sheep_spritesheet.png', 244, 200, 3);
+
+    this.load.audio('chickenSound', ['assets/audio/chicken.ogg', 'assets/audio/chicken.mp3']);
+    this.load.audio('horseSound', ['assets/audio/horse.ogg', 'assets/audio/horse.mp3']);
+    this.load.audio('pigSound', ['assets/audio/pig.ogg', 'assets/audio/pig.mp3']);
+    this.load.audio('sheepSound', ['assets/audio/sheep.ogg', 'assets/audio/sheep.mp3']);
   },
+
   create: function(){
 
     //--These 3 lines scale the game for all devices (responsive)-
@@ -26,30 +33,42 @@ var GameState = {
     this.background = this.game.add.sprite(0, 0, 'background');
 
     var animalData = [
-      { key: 'chicken', text: 'CHICKEN' },
-      { key: 'horse', text: 'HORSE' },
-      { key: 'pig', text: 'PIG' },
-      { key: 'sheep', text: 'SHEEP' }
+      { key: 'chicken', text: 'CHICKEN', audio: 'chickenSound' },
+      { key: 'horse', text: 'HORSE', audio: 'horseSound' },
+      { key: 'pig', text: 'PIG', audio: 'pigSound' },
+      { key: 'sheep', text: 'SHEEP', audio: 'sheepSound' }
     ];
-
+    //create a group to store all animals
     this.animals = this.game.add.group();
 
     var self = this;
 
     animalData.forEach(function(element){
-      animal = self.animals.create(-1000, self.game.world.centerY, element.key);
+      // create each animal and save its properties
+      animal = self.animals.create(-1000, self.game.world.centerY, element.key, 0);// the 0 at the end is which frame it uses from the sprite to create the animal
 
-      animal.customParams = { text: element.text };
+      // I'm saving everything that's not Phaser-related in an object
+      animal.customParams = { text: element.text, sound: self.game.add.audio(element.audio) };
+      // anchor point set to the center of the sprite
       animal.anchor.setTo(0.5);
 
+      // creating animal animation -- 
+      //   add animation('name', order of frames, frames/second, if animation should continue forever)
+      animal.animations.add('animate', [0,1,2,1,0,1], 3, false);
+
+
+      // enable input so we can touch it
       animal.inputEnabled = true;
       animal.input.pixelPerfectClick = true;
       animal.events.onInputDown.add(self.animateAnimal, self);
     });
 
-    // the next() method is a built in phaser group method animalData is a group
+    // the next() method is a built in phaser group method animalData is a group -- this places first animal in the middle
     this.currentAnimal = this.animals.next();
     this.currentAnimal.position.setTo(this.game.world.centerX, this.game.world.centerY);
+
+    // show Animal text
+    this.showText(this.currentAnimal);
 
     /*--------------------------
     // center of the world
@@ -107,23 +126,62 @@ var GameState = {
 
   },
   animateAnimal: function(sprite, event) {
-    console.log('animate the animal');
+    sprite.play('animate');
+    sprite.customParams.sound.play();
   },
   switchAnimal: function(sprite, event){
+
+    if(this.isMoving){
+      return false;
+    }
+
+    this.isMoving = true;
+
+    // hide text
+    this.animalText.visible = false;
+
     var newAnimal, endX;
 
     // get the direction of the arrow
     if (sprite.customParams.direction > 0) {
       newAnimal = this.animals.next();
+      newAnimal.x = -newAnimal.width / 2;
       endX = 640 + this.currentAnimal.width / 2;
     } else {
       newAnimal = this.animals.previous();
+      newAnimal.x = 640 + newAnimal.width / 2;
       endX = -this.currentAnimal.width / 2;
     }
 
-    this.currentAnimal.x = endX;
-    newAnimal.x = this.game.world.centerX;
+    newAnimalMovement = this.game.add.tween(newAnimal);
+    newAnimalMovement.to({ x: this.game.world.centerX }, 1000);
+    newAnimalMovement.onComplete.add(function(){
+      this.isMoving = false;
+      this.showText(newAnimal);
+    }, this);
+    newAnimalMovement.start();
+
+    currentAnimalMovement = this.game.add.tween(this.currentAnimal);
+    currentAnimalMovement.to({ x: endX }, 1000);
+    currentAnimalMovement.start();
+
+
+
     this.currentAnimal = newAnimal;
+  },
+  showText: function(animal) {
+    if (!this.animalText){
+      var style = {
+        font: 'bold 30pt Arial',
+        fill: '#D0171B',
+        align: 'center'
+      }
+      this.animalText = this.game.add.text(this.game.width / 2, this.game.height * 0.85, '', style);
+      this.animalText.anchor.setTo(0.5);
+    }
+
+    this.animalText.setText(animal.customParams.text);
+    this.animalText.visible = true;
   }
 };
 
