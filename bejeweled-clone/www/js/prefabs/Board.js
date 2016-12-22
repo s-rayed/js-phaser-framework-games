f2c51ea2adde1fca7150cc43ea31a3fe1f39f0d4
Rayed <siddiqui.rayed@gmail.com>
2016-12-22 14:22:12 -0500

    added swapping of blocks, and a check to see whether they are directly adjacent to eachother

--- bejeweled-clone/www/js/prefabs/Board.js
+++ bejeweled-clone/www/js/prefabs/Board.js
@@ -1,5 +1,8 @@
 var Match3 = Match3 || {};
 
+// The board has a visible Grid, and a RESERVE grid on top. the grids are 2 dimensional arrays. 
+// Once 3 gems are matched in the visible grid, gems will fall down from the reserve grid
+
 Match3.Board = function(state, rows, cols, blockVariations) {
     
     this.state = state;
@@ -90,5 +93,33 @@ Match3.Board.prototype.consoleLog = function() {
 
 }
 
-// The board has a visible Grid, and a RESERVE grid on top. the grids are 2 dimensional arrays. 
-// Once 3 gems are matched in the visible grid, gems will fall down from the reserve grid
+// Swapping blocks
+
+Match3.Board.prototype.swap = function(source, target) {
+
+    var temp = this.grid[target.row][target.col];
+
+    this.grid[target.row][target.col] = this.grid[source.row][source.col];
+    this.grid[source.row][source.col] = temp;
+
+}
+
+// Check if 2 blocks that are going to be swapped are adjacent to each other
+
+Match3.Board.prototype.checkAdjacent = function(source, target) {
+    var diffRow = Math.abs(source.row - target.row);
+    var diffCol = Math.abs(source.col - target.col);
+
+    var isAdjacent = (diffRow === 1 && diffCol === 0) || (diffRow === 0 && diffCol === 1)
+    return isAdjacent;
+}
+
+
+
+
+
+
+
+
+
+
